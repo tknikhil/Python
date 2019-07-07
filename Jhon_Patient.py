b0688fcbaf8d6ee2bf34d6a52e1a88c9887a4f0a
tknihil <nikhiltk02@gmail.com>
2019-07-08 00:44:01 +0530

    Displaying Name and age to the console

---
+++ Jhon_Patient.py
@@ -0,0 +1,7 @@
+name = "Jhon"
+age = 20
+is_Patient = True
+
+# Starting Phase Having Problem to Print
+# find Solution in PuondToKgConverter.py code
+print("Mr. " + name + " is " + str(age) + " years old and is he Our New Patient? ")
