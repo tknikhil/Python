534141a86cb6d36487acbf715248fad687393467
tknihil <nikhiltk02@gmail.com>
2019-07-08 00:39:23 +0530

    This Code Taking Input From User and displaying in Console

---
+++ Input_Question.py
@@ -0,0 +1,8 @@
+"""
+    Exercise 2
+This Code Taking Input From User and displaying in Console"""
+
+name = input("What is your Name? ")
+color = input("What is your Favourite Color? ")
+
+print("Hi " + name + " your favourite Color is " + color)
